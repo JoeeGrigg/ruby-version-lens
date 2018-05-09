f77eba7cf36d3940953d6ec4c2175f08b1c478e8
Joe Grigg <account@joegrigg.com>
2018-05-09 12:42:07 +0100

    Core functionality finished

--- extension.js
+++ extension.js
@@ -2,14 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const axios = require('axios');
-
-function retryFailedRequest (err) {
-	if (err.status != 200 && err.config && !err.config.__isRetryRequest) {
-	  err.config.__isRetryRequest = true;
-	  return axios(err.config);
-	}
-	throw err;
-  }
+const Promise = require('bluebird');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -49,41 +42,50 @@ function activate(context) {
 	}
 
     // Update Decorations
-	function updateDecorations() {
-		if (!activeEditor) {
-			return;
-		}
+	async function updateDecorations() {
+
+		// Retunr if not active editor
+		if (!activeEditor) { return; }
+
+		// Config
 		const regEx = /gem ["']([a-zA-z-_\d]+)["'], ["']([~><=\d.]+)["'].*/gm;
         const text = activeEditor.document.getText();
 		const upToDateLines = [];
 		const outOfDateLines = [];
 		const warningLines = [];
         let match;
-		let promises = [];
+		let promiseData = [];
+
+		// Setup Requests
 		while (match = regEx.exec(text)) {
-            let i = match 
-            promises.push(
-                axios.get(`https://rubygems.org/api/v1/gems/${i[1]}.json`)
-                .then(res => {
-                    const startPos = activeEditor.document.positionAt(i.index);
-					const endPos = activeEditor.document.positionAt(i.index + i[0].length);
-					if (res.data.version == i[2]) {
-                    	const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: `Up to date` };
-                    	upToDateLines.push(decoration);
-					} else {
-						const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: `Out of date` };
-						outOfDateLines.push(decoration);
-					}
-				})
-                .catch(err => { console.log(1) })
-            )
+            let i = match;
+            promiseData.push(i);
 		}
-  		axios.interceptors.response.use(undefined, retryFailedRequest);
-		Promise.all(promises).then(() => {
-			activeEditor.setDecorations(upToDateDecorationType, upToDateLines);
-			activeEditor.setDecorations(outOfDateDecorationType, outOfDateLines);
-			activeEditor.setDecorations(warningDecorationType, warningLines);
-		})
+
+		// Run requests and setup decorators decorators
+		for (const i of promiseData)  {
+			await Promise.delay(120);
+			await axios.get(`https://rubygems.org/api/v1/gems/${i[1]}.json`)
+					   	.then(res => {
+							const startPos = activeEditor.document.positionAt(i.index);
+							const endPos = activeEditor.document.positionAt(i.index + i[0].length);
+							if (res.data.version == i[2]) {
+							   	const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: `Up to date` };
+							   	upToDateLines.push(decoration);
+							} else {
+								const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: `Out of date` };
+								outOfDateLines.push(decoration);
+							}
+					   	})
+            		   	.catch(err => {
+					       	console.log(err);
+						});
+		}
+
+		// Add decorators
+		activeEditor.setDecorations(upToDateDecorationType, upToDateLines);
+		activeEditor.setDecorations(outOfDateDecorationType, outOfDateLines);
+		activeEditor.setDecorations(warningDecorationType, warningLines);
     }
     
 }
